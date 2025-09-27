bbe0334d206ecc09f63693da724cb60670f9fca6
Your Name <you@example.com>
2025-09-27 23:17:51 +0530

    changed to, fro and date, create new test for another route Bangalore to Nanded

--- src/test/java/tracker/RedbusPriceTrackerTest.java
+++ src/test/java/tracker/RedbusPriceTrackerTest.java
@@ -27,7 +27,7 @@ public class RedbusPriceTrackerTest {
 	
 	@Test
 	public void trackBusPrice()  throws Exception {
-   		System.out.println("===== Test started =====");
+   		System.out.println("===== Test 1 Bangalore to Hyderabad started =====");
    		ChromeOptions options = new ChromeOptions();
 
 		  
@@ -75,7 +75,7 @@ public class RedbusPriceTrackerTest {
     		
 
     		// Type into the focused element (React traps focus to a hidden input field)
-         sourceInput.sendKeys("Hyderabad");
+         sourceInput.sendKeys("Bangalore");
          File autoSuggest = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
  		FileUtils.copyFile(autoSuggest, new File("screenshots/debug_before_suggestclick.png"));
 
@@ -91,7 +91,7 @@ public class RedbusPriceTrackerTest {
     		if(fromSuggestion) {
     			driver.findElement(By.xpath("(//div[contains(@class,'listHeader')])[1]")).click();}
     		else {
-    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Hyderabad'])[1]")).click();
+    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Bangalore'])[1]")).click();
     		}
     		
     		
@@ -102,7 +102,7 @@ public class RedbusPriceTrackerTest {
     		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'searchSuggestionWrapper')]")));
     		
             WebElement DestinationInput = driver.switchTo().activeElement();
-            DestinationInput.sendKeys("Bangalore");
+            DestinationInput.sendKeys("Hyderabad");
     		boolean toSuggestion =false;
     		try {
     			toSuggestion=wait.until(ExpectedConditions.visibilityOfElementLocated(
@@ -115,7 +115,7 @@ public class RedbusPriceTrackerTest {
      		if(toSuggestion) {
      			driver.findElement(By.xpath("(//div[contains(@class,'listHeader')])[1]")).click();}
     		else {
-    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Bangalore'])[1]")).click();
+    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Hyderabad'])[1]")).click();
     			}
     		
     	//click on calendar
@@ -129,7 +129,7 @@ public class RedbusPriceTrackerTest {
     		
     		String Month =monthTextElement.getText();
     		System.out.println(Month+" old month");
-    		if(Month.toLowerCase().contains("august")) {
+    		if(Month.toLowerCase().contains("september")) {
     			WebElement arrow = driver.findElement(By.xpath("//i[contains(@class,'right__')]"));
     			wait.until(ExpectedConditions.elementToBeClickable(arrow)).click();
     			
@@ -140,7 +140,7 @@ public class RedbusPriceTrackerTest {
     		File dateClick = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
     		FileUtils.copyFile(dateClick, new File("screenshots/debug_before_dateclick.png"));
     		
-    		WebElement target = driver.findElement(By.xpath("//li//span[text()='6']"));
+    		WebElement target = driver.findElement(By.xpath("//li//span[text()='17']"));
 
     		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", target);
 
@@ -258,6 +258,226 @@ public class RedbusPriceTrackerTest {
     		  
     		
     		 }
+    		  
+    		  }
+    		  System.out.println("===== Test 1 Ended =====");
+    		  TelegramNotifier.sendTextMessage("------------------------");
+        } catch(Exception e) {
+        	System.out.println(e);
+        	TelegramNotifier.sendTextMessage("Exception Occured");
+        	TelegramNotifier.sendTextMessage(e.toString());
+        	
+        	
+        } finally {
+        	driver.quit();
+        }
+		 
+
+	}
+	@Test
+	public void NandedBusPricetracker()  throws Exception {
+   		System.out.println("===== Test 2 Bangalore to Nanded started =====");
+   		ChromeOptions options = new ChromeOptions();
+
+		  
+		
+		  if (System.getenv("CI") != null) {
+		  
+		  options.addArguments("--headless=new");
+		  options.addArguments("--disable-gpu"); options.addArguments("--no-sandbox");
+		  options.addArguments("--disable-dev-shm-usage");
+		  options.addArguments("--disable-blink-features=AutomationControlled");
+		  options.addArguments("--window-size=1920,1080");
+		  options.addArguments("start-maximized");
+		  options.addArguments("--force-device-scale-factor=1");
+		  options.addArguments("--high-dpi-support=1");
+		  options.
+		  addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+		  ); options.setExperimentalOption("excludeSwitches",
+		  Arrays.asList("enable-automation"));
+		  options.setExperimentalOption("useAutomationExtension", false);
+		 }
+		 
+		  WebDriver driver = new ChromeDriver(options);
+		  Actions actions = new Actions(driver);
+		 
+     
+		  driver.manage().window().setSize(new Dimension(1920, 1080));
+		String TrackerPrice = "";
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebDriverWait wait5 = new WebDriverWait(driver, Duration.ofSeconds(5));
+        try {
+ 
+    		//String link ="https://www.redbus.in/bus-tickets/bangalore-to-hyderabad?fromCityName=Bangalore&fromCityId=122&srcCountry=IND&fromCityType=CITY&toCityName=Hyderabad&toCityId=124&destCountry=India&toCityType=CITY&onward=14-Aug-2025&doj=14-Aug-2025&ref=home";
+
+    		String link = "https://www.redbus.in/";
+
+    		driver.get(link);
+    		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-autoid='searchWidget']")));
+    		// Click on the "From" field
+    		WebElement fromDiv = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'srcDestWrapper') and .//div[text()='From']]")));
+    		fromDiv.click();
+    		
+    		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'searchSuggestionWrapper')]")));
+    		
+         WebElement sourceInput = driver.switchTo().activeElement();
+    		
+
+    		// Type into the focused element (React traps focus to a hidden input field)
+         sourceInput.sendKeys("Bangalore");
+         File autoSuggest = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+ 		FileUtils.copyFile(autoSuggest, new File("screenshots/debug_before_suggestclick_2.png"));
+
+    		// Wait for and click on the auto-suggestion (optional safety)
+    		boolean fromSuggestion =false;
+    		try {
+    			fromSuggestion =wait.until(ExpectedConditions.visibilityOfElementLocated(
+        		    By.xpath("//div[contains(@class, 'searchCategory')][3]")	)).isDisplayed();}
+    		catch(Exception e) {
+    			
+    		}
+    		
+    		if(fromSuggestion) {
+    			driver.findElement(By.xpath("(//div[contains(@class,'listHeader')])[1]")).click();}
+    		else {
+    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Bangalore'])[1]")).click();
+    		}
+    		
+    		
+    		// Click on the "ToDiv" field
+    		WebElement toDiv = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'srcDestWrapper') and .//div[text()='To']]")));
+    		toDiv.click();
+    		
+    		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'searchSuggestionWrapper')]")));
+    		
+            WebElement DestinationInput = driver.switchTo().activeElement();
+            DestinationInput.sendKeys("Nanded");
+    		boolean toSuggestion =false;
+    		try {
+    			toSuggestion=wait.until(ExpectedConditions.visibilityOfElementLocated(
+        		    By.xpath("//div[contains(@class, 'searchCategory')][3]")	)).isDisplayed();}
+    		catch(Exception e) {
+    			
+    		}
+    		
+
+     		if(toSuggestion) {
+     			driver.findElement(By.xpath("(//div[contains(@class,'listHeader')])[1]")).click();}
+    		else {
+    			driver.findElement(By.xpath("(//div[contains(@class,'suggestion-item')]//div[text()='Nanded'])[1]")).click();
+    			}
+    		
+    	//click on calendar
+    		
+    		driver.findElement(By.xpath("//i[contains(@class,'icon-date_range')]")).click();
+    		wait.until(
+    				ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'datePickerWrapper')]")));
+    		
+    		WebElement monthTextElement = wait.until(
+    				ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(@class,'monthYear')]")));
+    		
+    		String Month =monthTextElement.getText();
+    		System.out.println(Month+" old month");
+    		if(Month.toLowerCase().contains("september")) {
+    			WebElement arrow = driver.findElement(By.xpath("//i[contains(@class,'right__')]"));
+    			wait.until(ExpectedConditions.elementToBeClickable(arrow)).click();
+    			
+    			String newMonth =driver.findElement(By.xpath("//p[contains(@class,'monthYear')]")).getText();
+    			System.out.println(newMonth+"new month");
+    		} 
+    		
+    		File dateClick = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    		FileUtils.copyFile(dateClick, new File("screenshots/debug_before_dateclick2.png"));
+    		
+    		WebElement target = driver.findElement(By.xpath("//li//span[text()='17']"));
+
+    		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", target);
+
+    		wait.until(ExpectedConditions.elementToBeClickable(target)).click();
+       		wait.until(
+    				ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'doj')]")));
+       		driver.findElement(By.xpath("//button[contains(@class,'searchButtonWrapper')]")).click();
+       		
+       	//	Thread.sleep(15000);
+       		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'invWrap__ind-search')]")));
+       	
+    		
+    		
+    		//div[@class="datePickerWrapper___9a8bba"]
+    		File src1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    		FileUtils.copyFile(src1, new File("screenshots/debug_before_busesFoundText.png"));
+    		
+    		WebElement busFoundText = driver.findElement(By.xpath("//div[contains(@class, 'busesFoundText')]"));
+
+    		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", busFoundText);
+    		wait.until(
+    				ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'busesFoundText')]")));
+    		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-autoid='filters-desktop']")));
+
+    		WebElement filterContainer = driver.findElement(By.xpath("//div[@data-autoid='filters-desktop']"));
+    		((JavascriptExecutor) driver).executeScript("arguments[0].scrollTop = arguments[0].scrollTop + 200;",
+    				filterContainer);
+
+    		
+    		
+    		
+
+    		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+    		
+    		ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+    		String currentTime = istTime.format(formatter);
+
+    		System.out.println("IST Time: " + currentTime);
+
+
+    		// Send with your message
+    		TelegramNotifier.sendTextMessage("📅 Sending data for Banaglore<=>Nanded" + currentTime);
+    		List<WebElement> newbusesList = driver.findElements(By.xpath("//div[contains(@class,'busTitleWrap_')]"));
+    		  for(int i=0;i<newbusesList.size();i++) { 
+    			  String busType=  newbusesList.get(i).findElement(By.xpath(".//p[contains(@class, 'busType__')]")).getText();
+    			  System.out.println(busType+ "  "+ i );
+    			  
+    		  TrackerPrice= newbusesList.get(i).findElement(By.xpath(".//ancestor::li//p[contains(@class,'finalFare')]")).getText();
+    		  
+    		  newbusesList.get(i).findElement(By.xpath(".//ancestor::li//button[contains(@class,'viewSeat')]")).click();
+    		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-autoid='busDetailsContainer']")));
+    		  
+    		  boolean isLoginVisible =false;
+    		  WebElement loginBottomsheet = null;
+      		try {
+      			 loginBottomsheet = 
+      					wait5.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-autoid='bottom-sheet']")));
+      			isLoginVisible = loginBottomsheet.isDisplayed();}
+      		catch(Exception e) {
+      			
+      		}
+      
+       		if(isLoginVisible) {
+       			loginBottomsheet.findElement(By.xpath(".//button[contains(@class,'actionButton')]")).click();}
+ 
+    	
+              // Screenshot
+              File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            
+              File dest = new File("screenshots/redbus_price_result_BangToNan" + i + ".png");
+              FileUtils.copyFile(src, dest);
+
+              System.out.println("Screenshot saved to /screenshots");
+              driver.findElement(By.xpath("//div[@class=\"header__ind-seat-styles-module-scss-4q84B\"]//button")).click();
+            //div[@class="header__ind-seat-styles-module-scss-4q84B"]//button
+    		
+              // Send message
+              TelegramNotifier.sendTextMessage("Bus Price is :"+TrackerPrice);
+
+              // Send screenshot
+              TelegramNotifier.sendPhoto(dest);
+              Thread.sleep(1000);
+
+              System.out.println("Price and screenshot sent to Telegram!");
+
+    		  
+    		
+    		
     		  
     		  }
     		  System.out.println("===== Test Ended =====");
